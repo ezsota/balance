f8a339e3fdb9272af0a554976368d55ce14d18aa
ET-Mini <edison.lto@outlook.com>
2026-02-20 09:00:36 -0500

    Updated comments in server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-// Load environment variables (.env)
+// Load environment variables from server/.env
 dotenv.config();
 
 // Init Express, CORS, and JSON HTTP reqs
@@ -12,17 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 // MONGO_URI error handler
+// (MONGO_URI defined in server/.env)
 if (!process.env.MONGO_URI) {
     console.error("MONGO_URI is not defined, check environment variables");
     process.exit(1); // terminate process
 };
 
-// Connect to mongo server using MONGO_URI (.env)
+// Connect to mongo server using via mongoose
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch(error => console.error(`Error connecting to MongoDB: ${error}`));
 
-// Start server on specified port (.env) or fallback
+// Start server on specified port or fallback
+// (PORT defined in server/.env)
 const PORT = process.env.APP_SERVER_PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
