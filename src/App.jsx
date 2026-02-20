0bd862aee6307c2f3faa1b6613fff84f4bed90d8
ET-Mini <edison.lto@outlook.com>
2026-02-20 08:38:20 -0500

    Added google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>BALANCE</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum harum blanditiis, ipsam aspernatur dolor animi beatae nobis, eum, nihil at pariatur. Eligendi fuga numquam neque corporis illo maiores modi deserunt?</p>
+      <footer><p>Footer</p></footer>
+    </>
   )
 }
 
