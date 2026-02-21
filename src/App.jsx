dfa75c9dbfa9f75eb4de050ba36d4b50f34268ff
ET-Mini <edison.lto@outlook.com>
2026-02-21 12:15:16 -0500

    Updated frontend routing / routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-// Pages:
+// Layouts:
 import DashboardLayout from "./layouts/DashboardLayout";
+// Pages:
+import Overview from "./pages/Overview.jsx";
+import Transactions from "./pages/Transactions.jsx";
+import Reports from "./pages/Reports.jsx";
+import Goals from "./pages/Goals.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<DashboardLayout />}>
-        <Route path="/" element={<ExpenseLanding />}/>
-        <Route path="/component1" element={<component1/>/>}/>
+        <Route index element={<Overview />} />
+        <Route path="/transactions-view" element={<Transactions />} />
+        <Route path="/reports-view" element={<Reports />} />
+        <Route path="/goals-view" element={<Goals />} />
       </Route>
     </Routes>
   )
