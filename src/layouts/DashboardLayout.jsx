1d6c8a49e8080a356d3efdaf46799dbe81bc389a
ET-Mini <edison.lto@outlook.com>
2026-02-23 09:57:00 -0500

    Updated frontend dashboard layout and favicon

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -4,11 +4,19 @@ import Footer from "../components/Footer.jsx";
 
 export default function DashboardLayout() {
     return (
-        <div>
-            <nav>
+        <div className="container-fluid row p-0 m-0 vh-100">
+            <nav className="
+            nav 
+            bg-green 
+            col-12 col-md-2 
+            d-flex flex-column
+            ">
                 <Navbar />
             </nav>
-            <main>
+            <main className="
+            col-12 col-md-10 
+            d-flex flex-column flex-fill
+            ">
                 <Outlet />
                 <footer>
                     <Footer />
