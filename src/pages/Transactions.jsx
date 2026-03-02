f2b538adc09f2aa66bafd32aa442484cf92bd90c
ET-Mini <edison.lto@outlook.com>
2026-03-02 11:36:49 -0500

    Undid transactions / transactionlist changes

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -12,22 +12,10 @@ Components: TransactionInputs.jsx + Transaction List.jsx
 
 */
 
-import { useState, useEffect } from "react";
-import { getTransactions } from "../api/backendApi.js";
 import TransactionInputBox from "../components/TransactionInputBox.jsx";
 import TransactionList from "../components/TransactionList.jsx";
 
 export default function Transactions() {
-    // Initial list transactions (all)
-    const [transactions, setTransactions] = useState([]);
-    //Filtered list transactions
-    const [filteredTransactions, setFilteredTransactions] = useState([])
-
-    // Get all transactions on page render
-    useEffect(() => {
-        getTransactions().then(setTransactions).catch(console.error);
-    }, []);
-
     return (
         <div className="d-flex flex-column justify-content-center w-100">
             {/* HEADER */}
