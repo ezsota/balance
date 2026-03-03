93b06fc85f8fee3a11fb53c75fc3b652d2f87b0d
ET-Mini <edison.lto@outlook.com>
2026-03-03 12:06:58 -0500

    Updated Mongoose schema to harden security

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -3,14 +3,46 @@
 // ===============
 import mongoose from "mongoose";
 
-// Define model schema via mongoose:
+// Define model schema via mongoose
+// Restrictions avoid script insertion and invalid data
+// Categories must match src/helpers/categoryGroups.js
+// Force UTC default to avoid timezone bugs
 const transactionSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    amount: { type: Number, required: true },
-    category: { type: String, required: true },
-    // Force UTC default to avoid timezone bugs
-    date: { type: Date, default: () => new Date() }
+    title: { type: String, required: true, trim: true, maxlength: 100 },
+    amount: {
+        type: Number, required: true,
+        validate: {
+            validator: v => v !== 0 && Number.isFinite(v),
+            message: "Amount can't be zero"
+        }
+    },
+    category: {
+        type: String, required: true, enum: [
+        "Business",
+        "Insurance",
+        "Payments",
+        "Savings",
+        "Childcare",
+        "Clothing",
+        "Education",
+        "Food",
+        "Housing",
+        "Transportation",
+        "Utilities",
+        "Entertainment",
+        "Occasions",
+        "Recreation",
+        "Social",
+        "Vacations",
+        "Gifts",
+        "Memberships",
+        "Pets",
+        "Seasonal",
+        "Subscriptions"
+        ]
+    },
+    date: { type: Date, default: () => new Date(), required: true }
 });
 
-// Export model as "Transaction" for MongoDB:
+// Export model as "Transaction" to MongoDB:
 export default mongoose.model("Transaction", transactionSchema);
