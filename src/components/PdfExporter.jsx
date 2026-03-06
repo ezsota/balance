a4fc1643b8f5c69ad88791068444c1cf04c8b680
ET-Mini <edison.lto@outlook.com>
2026-03-06 11:59:58 -0500

    Un-Installed @react-pdf/renderer

--- src/components/PdfExporter.jsx
+++ src/components/PdfExporter.jsx
@@ -1,7 +1,23 @@
-export default function PdfExporter() {
+import ReactPDF from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+
+export default function PdfExporter(props) {
+    const MyDocument = () => (
+        <Document>
+            <Page size="A4" style={styles.page}>
+                {Reports}
+            </Page>
+        </Document>
+    );
+
+    function handleClick() {
+        console.log('Saving to PDF');
+        ReactPDF.render(<MyDocument />, `/reports-view`);
+    }
+
     return (
-    <button className="btn btn-success bg-green w-100 mt-4 mt-md-2">
-        Export to PDF
-    </button>
+        <button className="btn btn-success bg-green w-100 mt-4 mt-md-2" onClick={handleClick}>
+            Export to PDF
+        </button>
     )
 };
