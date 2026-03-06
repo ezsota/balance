8c71c65505dabf85e13c095c778609af85c96b1c
ET-Mini <edison.lto@outlook.com>
2026-03-06 11:21:02 -0500

    Added error icon

--- src/pages/Errors.jsx
+++ src/pages/Errors.jsx
@@ -1,10 +1,14 @@
 import { useLocation } from "react-router-dom";
+import errorsIcon from "../assets/errors-icon.svg";
 
 export default function Errors() {
     // Get error message state
     const { state } = useLocation();
 
     return (
-        <h1>{state || "Something went wrong."}</h1>
+        <section className="h-100 d-flex flex-column justify-content-center align-items-center">
+            <img src={errorsIcon} style={{width: "50px"}} alt="Error icon" />
+            <h1>{state || "Something went wrong."}</h1>
+        </section>
     )
 };
