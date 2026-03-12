4cee5fe7580fa4509533503718cc1f89b1343ef9
ET-Mini <edison.lto@outlook.com>
2026-03-12 09:55:26 -0400

    Testing useContext for global transaction data

---
+++ src/context/TransactionContext.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect, createContext, useContext } from "react";
+import { getTransactions } from "../api/backendApi.js";
+
+// Init context()
+const TransactionContext = createContext();
+
+export function TransactionContextProvider({ children }) {
+    // Context transaction state (will be used globally)
+    const [transactions, setTransactions] = useState([]);
+
+    // Async set context state to transaction data
+    async function loadTransactions() {
+        try {
+            const data = await getTransactions();
+            setTransactions(data);
+        } catch (error) {
+            console.error("Transaction context provider failed to retrieve data", error);
+        }
+    }
+
+    // Async get transaction data on render
+    useEffect(() => {
+        const fetchData = async () => {
+            await loadTransactions();
+        };
+
+        fetchData();
+    }, []);
+
+    // Provide transaction state and async funcs to app
+    return (
+        <TransactionContext.Provider
+            value={{
+                transactions,
+                setTransactions,
+                refreshTransactions: loadTransactions
+            }}
+        >
+            {children}
+        </TransactionContext.Provider>
+    );
+}
+
+// Export above context
+export function useTransactionContext() {
+    return useContext(TransactionContext);
+}
